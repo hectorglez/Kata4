d9d18ebcc7735e9a8118c434470e1d000e0de578
usuario <usuario@10.22.143.11>
2015-10-29 09:16:57 +0000

    version 3

--- src/kata4/Main.java
+++ src/kata4/Main.java
@@ -1,20 +1,43 @@
 
 package kata4;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
 
 
 public class Main {
 
     
-    public static void main(String[] args) {
-       String pathname = "C:\\Users\\usuario\\Documents";
-       File file = new File (pathname);
-       String [] dire = file.list();
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String pathname = "C:\\Users\\usuario\\Documents";
+        File file = new File (pathname);
+        /*String [] dire = file.list();
         for (String dire1 : dire) {
             System.out.println(dire1);
         }
-        print(file.listFiles(),"");
+        print(file.listFiles(),"");*/
+        String from="C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpg";
+        String to="C:\\Users\\usuario\\Desktop\\Nueva carpeta\\bb2";
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        Date date = new Date();
+        while (true){
+            int read = input.read();
+            if(read<0)break;
+            output.write(read);
+        }
+        System.out.println(new Date().getTime()-date.getTime()+ " msg");
+        input.close();
+        output.flush();
+        output.close();
     }
 
     private static void print(File[] listFiles, String string) {
@@ -22,7 +45,7 @@ public class Main {
             return;
         }
         for (File listFile : listFiles) {
-            System.out.println(string +(listFile.isDirectory()? "+": " ")+ listFile.getName());
+            System.out.println(string +(listFile.isDirectory()? "+": "--> ")+ listFile.getName());
             if(listFile.isFile() || listFile.isHidden())continue;
             print(listFile.listFiles(),string+" ");
         }
