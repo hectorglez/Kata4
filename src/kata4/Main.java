a714119e434edc3a20b0b24f39dc37c07bea3308
usuario <usuario@10.22.143.11>
2015-10-29 10:08:57 +0000

    version 6 parte mails

--- src/kata4/Main.java
+++ src/kata4/Main.java
@@ -12,6 +12,20 @@ public class Main {
         String nameFile = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\DATA\\emailsfilev1.txt";
         ArrayList<String> mailArray  = MailListReader.read(nameFile);
         System.out.println(mailArray.size());
+        
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("gmail");
+        histogram.increment("gmail");
+        histogram.increment("gmail");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        histogram.increment("ulpgc");
+        
+        new HistogramDisplay(histogram).execute();
+        
+        
     }
 
     
