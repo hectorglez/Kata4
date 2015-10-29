9effec8ade5c48cbd973a98ea10aa280474852bf
usuario <usuario@10.22.143.11>
2015-10-29 08:59:13 +0000

    version 2

--- src/kata4/Main.java
+++ src/kata4/Main.java
@@ -14,6 +14,18 @@ public class Main {
         for (String dire1 : dire) {
             System.out.println(dire1);
         }
+        print(file.listFiles(),"");
+    }
+
+    private static void print(File[] listFiles, String string) {
+        if(listFiles == null){
+            return;
+        }
+        for (File listFile : listFiles) {
+            System.out.println(string +(listFile.isDirectory()? "+": " ")+ listFile.getName());
+            if(listFile.isFile() || listFile.isHidden())continue;
+            print(listFile.listFiles(),string+" ");
+        }
     }
     
 }
